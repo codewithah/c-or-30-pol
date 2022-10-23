278b9a6e44e9f5ac6f3a8d915d8376cc4832a839
codewithah <codewithah@gmail.com>
2022-10-23 18:38:37 +0330

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './auth/login/login.component';
+import {FormsModule} from "@angular/forms";
+import { EditProfileComponent } from './dashboard/edit-profile/edit-profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    EditProfileComponent,
+  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
